cd11d1911f316bb09751272d092f8ddff1aa7d5d
anson171 <250139770@stu.vtc.edu.hk>
2026-09-23 16:04:35 +0800

    app.py
    Removed 'Brand' from the FEATURES list to prevent crashes during training.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 
 # CHANGE THIS LINE. Use your own column names. Do not leave the words PASTE / HERE.
 # Do not add Displacement_cc. Empty engine cc will crash training.
-FEATURES = ['Manufacture_Year', 'Mileage_km', 'Horsepower_PS', 'Brand']         # Example: ["Manufacture_Year", "Mileage_km"]
+FEATURES = ['Manufacture_Year', 'Mileage_km', 'Horsepower_PS']         # Example: ["Manufacture_Year", "Mileage_km"]
 RANDOM_STATE = 42   # public demo — does not need to match your Student ID
 
 @st.cache_data
